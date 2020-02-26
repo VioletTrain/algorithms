8701e01ded40e283adf241b13d5c1e89738fe61e
Andrij Kshanovskij <andrewafk2000@gmail.com>
2020-02-26 11:00:51 +0200

    Mark Application interface as resolved after creating HttpApp object

--- public/index.php
+++ public/index.php
@@ -7,10 +7,13 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Anso\Framework\Base\Configuration;
 use Anso\Framework\Base\Container;
+use Anso\Framework\Contract\Application;
 use Anso\Framework\Http\HttpApp;
 
 $configuration = new Configuration('/config/http');
 $container = new Container($configuration);
 $app = new HttpApp($container, $configuration);
+
+$container->addResolved(Application::class, $app);
 
 $app->start();
